0b656b8d85cda4ca5c663f9e8514af8bebd16dba
Sean Houde <seanhoude@gmail.com>
2018-11-14 23:23:59 -0500

    Finished chapter 7

--- collection/admin.py
+++ collection/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from collection.models import ClimbingShoe
 
+
 class ClimbingShoeAdmin(admin.ModelAdmin):
     model = ClimbingShoe
     list_display = ('name', 'description',)
     prepopulated_fields = {'slug': ('name',)}
+
 
 admin.site.register(ClimbingShoe, ClimbingShoeAdmin)
--- collection/views.py
+++ collection/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from collection.models import ClimbingShoe
 
 # Create your views here.
 def index(request):
-    number = 6
-    thing = "Sean Houde"
+    ClimbingShoes = ClimbingShoe.objects.all()
     return render(request, 'index.html', {
-        'number': number,
-        'thing': thing,
+        'ClimbingShoes': ClimbingShoes,
     })
+
+    # number = 6
+    # thing = "Sean Houde"
+    # return render(request, 'index.html', {
+    #     'number': number,
+    #     'thing': thing,
+    # })
